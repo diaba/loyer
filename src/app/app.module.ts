e2dadabdc3e077f57f4f81c954d3dd97571fe7fc
diaba <hwar@cincinnatistate.edu>
2022-02-08 19:37:33 -0500

    import { MatSliderModule } from '@angular/material/slider'; and add it to import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    MatSliderModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
